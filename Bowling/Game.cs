a54d6cfe26834169f161a806f91fb66a7a2bbb18
swapnil <swapnilpande96@gmail.com>
2014-07-15 11:09:23 +0530

    Added stubs for bowling game.

---
+++ Bowling/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    public class Game
+    {
+        public void Roll(int pins)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetScore()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ BowlingFixtures/GameFixture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingFixtures
+{
+    [TestClass]
+    public class GameFixture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
