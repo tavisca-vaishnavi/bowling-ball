bf7850a6f1c5084502755589f35bc992c6cfeb01
Vaishnavi <vgalgali@tavisca.com>
2014-07-15 19:21:40 +0530

    changes by vaishnavi

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,14 +8,89 @@ namespace Bowling
 {
     public class Game
     {
+        public int Final = 0; int i = 0;
+        public int[] arr_pins = new int[22];
+        int b1 = 0, b2 = 0;
+        int origin = 0;
+        //int flag1 = 0;//even & odd
+        //int flag2 = 0;//no of pin=10
+        //int flag3 = 0;//sum from both rolls=10
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
-        }
+            if (pins == 10 && i % 2 == 0 && i <= 19)
+            {
+                arr_pins[i] = pins;
+                i++;
+                arr_pins[i] = 0;
+                i++;
+            }
+            else if (pins == 10 && i % 2 != 0 && i > 19)
+            {
+                arr_pins[i] = pins;
 
+            }
+            else if (pins == 10 && i % 2 == 0 && i > 19)
+            {
+                arr_pins[i] = pins;
+                i++;
+            }
+            else
+            {
+                arr_pins[i] = pins;
+                i++;
+            }
+
+
+
+
+
+        }
         public int GetScore()
         {
-            throw new NotImplementedException();
+            b1 = arr_pins[20];
+            b2 = arr_pins[21];
+
+
+            origin = arr_pins[0];
+            for (int i = 0; i < 20; i++)
+            {
+                Final += arr_pins[i];
+            }
+            for (int i = 0; i < 18; i = i + 2)
+            {
+                if ((arr_pins[i] + arr_pins[i + 1] == 10) && arr_pins[i] != 10)
+                {
+                    Final += arr_pins[i + 2];
+                }
+            }
+            if ((arr_pins[18] + arr_pins[19] == 10) && arr_pins[18] != 10)
+            {
+                Final += b1;
+            }
+            for (int i = 0; i < 16; i++)
+            {
+                if (i % 2 == 0 && arr_pins[i] == 10)
+                {
+                    if (arr_pins[i + 2] == 10)
+                        Final += arr_pins[i + 2] + arr_pins[i + 4];
+                    else
+                        Final += arr_pins[i + 3] + arr_pins[i + 2];
+
+                }
+            }
+            if (arr_pins[16] == 10)
+            {
+                if (arr_pins[18] == 10)
+                    Final += arr_pins[18] + b1;
+                else
+                    Final += arr_pins[18] + arr_pins[19];
+            }
+            if (arr_pins[18] == 10)
+            {
+                Final += b1 + b2;
+            }
+            return Final;
+            // throw new NotImplementedException();
         }
 
     }
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -1,14 +1,183 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Bowling;
 namespace BowlingFixtures
 {
     [TestClass]
     public class GameFixture
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NoSpareNoStrike()
         {
+            Game game = new Game();
+            game.Roll(1);
+            game.Roll(2);
+
+            game.Roll(3);
+            game.Roll(4);
+
+            game.Roll(5);
+            game.Roll(4);
+
+            game.Roll(3);
+            game.Roll(2);
+
+            game.Roll(1);
+            game.Roll(0);
+
+            game.Roll(1);
+            game.Roll(2);
+
+            game.Roll(3);
+            game.Roll(4);
+
+            game.Roll(5);
+            game.Roll(4);
+
+            game.Roll(3);
+            game.Roll(2);
+
+            game.Roll(0);
+            game.Roll(1);
+
+
+            System.Diagnostics.Debug.WriteLine("value :" + game.GetScore());
+        }
+
+        [TestMethod]
+        public void OnlySpare()
+        {
+            Game game = new Game();
+            game.Roll(1);
+            game.Roll(9);
+
+            game.Roll(1);
+            game.Roll(0);
+
+            game.Roll(3);
+            game.Roll(7);
+
+            game.Roll(2);
+            game.Roll(0);
+
+            game.Roll(5);
+            game.Roll(5);
+
+            game.Roll(3);
+            game.Roll(0);
+
+            game.Roll(7);
+            game.Roll(3);
+
+            game.Roll(4);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(0);
+            game.Roll(1);
+
+
+            System.Diagnostics.Debug.WriteLine("value :" + game.GetScore());
+        }
+        [TestMethod]
+        public void OnlyStrike()
+        {
+            Game game = new Game();
+            game.Roll(10);
+
+
+            game.Roll(10);
+
+
+            game.Roll(10);
+
+
+            game.Roll(10);
+
+
+            game.Roll(10);
+
+
+            game.Roll(10);
+
+
+            game.Roll(10);
+
+
+            game.Roll(10);
+
+
+            game.Roll(10);
+
+
+            game.Roll(10);
+
+            game.Roll(0);
+            game.Roll(0);
+
+            System.Diagnostics.Debug.WriteLine("value :" + game.GetScore());
+        }
+        [TestMethod]
+        public void AllStrike()
+        {
+            Game game = new Game();
+            game.Roll(10);
+            game.Roll(10);
+
+            game.Roll(10);
+            game.Roll(10);
+
+            game.Roll(10);
+            game.Roll(10);
+
+            game.Roll(10);
+            game.Roll(10);
+
+            game.Roll(10);
+            game.Roll(10);
+
+            game.Roll(10);
+            game.Roll(10);
+
+            System.Diagnostics.Debug.WriteLine("value :" + game.GetScore());
+        }
+
+        [TestMethod]
+        public void Random()
+        {
+            Game game = new Game();
+            game.Roll(1);
+            game.Roll(6);
+
+            game.Roll(10);
+
+            game.Roll(5);
+            game.Roll(5);
+
+            game.Roll(4);
+            game.Roll(5);
+
+            game.Roll(10);
+
+            game.Roll(1);
+            game.Roll(1);
+
+            game.Roll(6);
+            game.Roll(4);
+
+            game.Roll(10);
+
+            game.Roll(0);
+            game.Roll(10);
+
+            game.Roll(10);
+
+            game.Roll(5);
+            game.Roll(5);
+
+            System.Diagnostics.Debug.WriteLine("value :" + game.GetScore());
         }
     }
 }
